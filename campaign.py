2f95d0a5e31d0d47a6aced83ce9214cc8569e436
sakabmw <sakabumi.wahyudi@gmail.com>
2021-04-03 18:58:08 +0700

    Delete list to simplify the code.

--- campaign.py
+++ campaign.py
@@ -64,52 +64,8 @@ fields = [
     Campaign.Field.upstream_events
 ]
 
-list_keys = [
-    'account_id',
-    'ad_strategy_id',
-    'adbatch',
-    'adlabels',
-    'bid_strategy',
-    'boosted_object_id',
-    'brand_lift_studies',
-    'budget_rebalance_flag',
-    'budget_remaining',
-    'buying_type',
-    'can_create_brand_lift_study',
-    'can_use_spend_cap',
-    'configured_status',
-    'created_time',
-    'daily_budget',
-    'effective_status',
-    'execution_options',
-    'id',
-    'is_skadnetwork_attribution',
-    'issues_info',
-    'iterative_split_test_configs',
-    'last_budget_toggling_time',
-    'lifetime_budget',
-    'name',
-    'objective',
-    'pacing_type',
-    'promoted_object',
-    'recommendations',
-    'smart_promotion_type',
-    'source_campaign',
-    'source_campaign_id',
-    'special_ad_categories',
-    'special_ad_category',
-    'special_ad_category_country',
-    'spend_cap',
-    'start_time',
-    'status',
-    'stop_time',
-    'topline_id',
-    'updated_time',
-    'upstream_events'
-]
-
 # Creating a blank dictionary for storing the data
-data = {key:[] for key in list_keys}
+data = {key:[] for key in fields}
 
 # Getting the data
 campaigns = my_account.get_campaigns(fields=fields)
